8752807cda89e1b2e7a98a6049961c88016c5292
Lim-Juri <imjuly05@gmail.com>
2023-09-22 12:04:43 +0900

    [FEAT] : ⚙️: Search 앱 만들기 - 개인과제

--- SearchApp/app/src/main/java/com/example/search/BookmarkFragment.kt
+++ SearchApp/app/src/main/java/com/example/search/BookmarkFragment.kt
@@ -2,11 +2,10 @@ package com.example.search
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.search.data.KakaoImage
 import com.example.search.databinding.FragmentBookmarkBinding
@@ -14,7 +13,10 @@ import com.example.search.databinding.FragmentBookmarkBinding
 class BookmarkFragment : Fragment() {
 
     private lateinit var mContext: Context
-    private var binding: FragmentBookmarkBinding? = null
+    private var _binding: FragmentBookmarkBinding? = null
+    private val binding
+        get() = _binding!!
+
     private lateinit var adapter: BookmarkAdapter
     private var likedItems: List<KakaoImage> = listOf()
 
@@ -27,24 +29,37 @@ class BookmarkFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        _binding = FragmentBookmarkBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setItems()
+    }
+
+    private fun setItems() {
         val fetchlike = activity as MainActivity
         likedItems = fetchlike.likedItems
-
-        Log.d("BookmarkFragment", "likedItems size = ${likedItems.size}")
-
-        adapter = BookmarkAdapter(mContext).apply {
-            items = likedItems.toMutableList()
-        }
-
-        adapter.notifyDataSetChanged()
-
-        binding = FragmentBookmarkBinding.inflate(inflater, container, false).apply {
-            bookmarkRecyclerview.layoutManager =
+        if (::adapter.isInitialized) {
+            adapter.items = fetchlike.likedItems
+            adapter.notifyDataSetChanged()
+        } else {
+            adapter = BookmarkAdapter(mContext).apply {
+                items = likedItems.toMutableList()
+            }
+            binding.bookmarkRecyclerview.layoutManager =
                 StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
-            bookmarkRecyclerview.adapter = adapter
+            binding.bookmarkRecyclerview.adapter = adapter
         }
+    }
 
-        return binding?.root
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
+
+
+
